9cd7c46a7a20a3350c923f6b8c8c81a48aa4bbfd
rogi-rogi <yrkim6883@gmail.com>
2025-10-31 12:50:57 +0900

    [init] - jdk 17 - db 추가 검토 필요 postgresql -> mongodb

---
+++ src/main/java/gdgoc/everyclub/EveryClubApplication.java
@@ -0,0 +1,13 @@
+package gdgoc.everyclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryClubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EveryClubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gdgoc/everyclub/EveryClubApplicationTests.java
@@ -0,0 +1,13 @@
+package gdgoc.everyclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EveryClubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
